cd96fc4121e3dc27ca4ddb91abc64d3c93835da7
Yiannis Ntakoumis <giannisdak@gmail.com>
2025-04-18 18:12:24 +0300

    New component person table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Ioannis';
+
+  person = {
+    givenName: 'Ioannis',
+    surName: 'Ntakoumis',
+    age: 33,
+    email: 'intakoumis@aueb.gr',
+  };
 }
